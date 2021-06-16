c02c66b76d73c0f032ec12fb29990f2b11218f5c
AnWonYoung <ko1529@naver.com>
2021-06-16 23:45:48 +0900

    홀짝

---
+++ java_practice/src/algorithm/baekjoon/Main2.java
@@ -0,0 +1,39 @@
+package algorithm.baekjoon;
+
+import java.util.Scanner;
+
+public class Main2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+
+		if (num % 4 == 0) {
+			if(num % 400 == 0 || num % 100 != 0) {
+				System.out.println(1);
+			}else {
+			System.out.println(0);
+		} 
+		} else {
+			System.out.println(0);
+		}
+	}
+
+}
+
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		Scanner in = new Scanner(System.in);
+//		
+//		int y = in.nextInt();
+//		in.close();
+//       
+//		if(y%4==0) {
+//			if(y%400==0) System.out.println("1");
+//			else if(y%100==0) System.out.println("0");
+//			else System.out.println("1");
+//		}
+//		else System.out.println("0");
+//	}
+//}
+
